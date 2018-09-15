0c0fe86aaf8e90e2e8a85278acc52d8ee0b48d0d
sara <avasara89@gmail.com>
2018-09-16 00:01:31 +0600

    commit 3

--- app/src/main/java/com/example/sara/jobandtutionfinder/profilefrag.java
+++ app/src/main/java/com/example/sara/jobandtutionfinder/profilefrag.java
@@ -1,10 +1,36 @@
 package com.example.sara.jobandtutionfinder;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class profilefrag extends AppCompatActivity{
+
+    private static final String TAG = "ViewDatabase";
+
+    //add Firebase Database stuff
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
+    private String userID;
+
+    private ListView mListView;
 
 
     @Override
@@ -12,5 +38,89 @@ public class profilefrag extends AppCompatActivity{
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_profile);
+
+        mListView = (ListView) findViewById(R.id.listview);
+
+        //declare the database reference object. This is what we use to access the database.
+        //NOTE: Unless you are signed in, this will not be useable.
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+        FirebaseUser user = mAuth.getCurrentUser();
+        userID = user.getUid();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                    Toast.makeText(profilefrag.this, "successfully signed in", Toast.LENGTH_LONG).show();
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+
+                    //Toast.makeText(profilefrag.this, "successfully sign out", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        };
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                showData(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        });
     }
+    private void showData(DataSnapshot dataSnapshot) {
+        for(DataSnapshot ds : dataSnapshot.getChildren()){
+            User uInfo = new User();
+            uInfo.setName(ds.child(userID).getValue(User.class).getName()); //set the name
+            uInfo.setEmail(ds.child(userID).getValue(User.class).getEmail()); //set the email
+            uInfo.setStudentid(ds.child(userID).getValue(User.class).getStudentid()); //set the student_id
+
+            //display all the information
+            Log.d(TAG, "showData: name: " + uInfo.getName());
+            Log.d(TAG, "showData: email: " + uInfo.getEmail());
+            Log.d(TAG, "showData: phone_num: " + uInfo.getStudentid());
+
+            ArrayList<String> array  = new ArrayList<>();
+            array.add(uInfo.getName());
+            array.add(uInfo.getEmail());
+            array.add(uInfo.getStudentid());
+            ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,array);
+            mListView.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+
+
 }
+
