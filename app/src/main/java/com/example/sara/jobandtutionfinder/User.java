80b2b4a0ac3ad51a173c08a94048da1fd6975453
sara <avasara89@gmail.com>
2018-09-15 22:39:57 +0600

    commit 1

--- app/src/main/java/com/example/sara/jobandtutionfinder/User.java
+++ app/src/main/java/com/example/sara/jobandtutionfinder/User.java
@@ -7,10 +7,37 @@ public class User {
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStudentid() {
+        return studentid;
+    }
+
+    public void setStudentid(String studentid) {
+        this.studentid = studentid;
+    }
+
     public User(String name, String email, String studentid) {
         this.name = name;
         this.email = email;
         this.studentid = studentid;
+
+
+
 
     }
 }
