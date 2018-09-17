ed7061fb87b2a025c3f58f8b04e21ea55be1017f
sara <avasara89@gmail.com>
2018-09-17 21:49:39 +0600

    commit pass_change1

--- app/src/main/java/com/example/sara/jobandtutionfinder/changePass.java
+++ app/src/main/java/com/example/sara/jobandtutionfinder/changePass.java
@@ -1,4 +1,13 @@
 package com.example.sara.jobandtutionfinder;
 
-public class changePass {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class changePass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_pass);
+    }
 }
